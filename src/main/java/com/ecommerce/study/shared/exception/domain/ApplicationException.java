a13cd112822b0304b7fc8eb677637b21cd7e1985
Yudisssss <gabrielyudi06@gmail.com>
2026-09-04 01:45:12 -0300

    feat(src): create exception in all code

--- src/main/java/com/ecommerce/study/shared/exception/domain/ApplicationException.java
+++ src/main/java/com/ecommerce/study/shared/exception/domain/ApplicationException.java
@@ -1,4 +1,21 @@
 package com.ecommerce.study.shared.exception.domain;
 
-public class ApplicationException {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ApplicationException extends RuntimeException{
+    private final int statusCode;
+    private final String message;
+    private final String details;
+
+    public ApplicationException(int statusCode, String message, String details){
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.details = details;
+    }
 }
--- src/main/java/com/ecommerce/study/shared/exception/domain/ResponseError.java
+++ src/main/java/com/ecommerce/study/shared/exception/domain/ResponseError.java
@@ -1,4 +1,20 @@
 package com.ecommerce.study.shared.exception.domain;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseError {
+    private LocalDateTime timestamp;
+    private String status;
+    private int statusCode;
+    private String error;
+    private String message;
+    private String path;
+    private String details;
 }
