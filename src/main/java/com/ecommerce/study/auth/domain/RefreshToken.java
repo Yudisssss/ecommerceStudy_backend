7f651ee49bee5e044a05a09d668ace2d34fb7806
Yudisssss <gabrielyudi06@gmail.com>
2026-09-04 01:46:37 -0300

    feat(auth): add base for create auth in src code

--- src/main/java/com/ecommerce/study/auth/domain/RefreshToken.java
+++ src/main/java/com/ecommerce/study/auth/domain/RefreshToken.java
@@ -1,4 +1,19 @@
 package com.ecommerce.study.auth.domain;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
 public class RefreshToken {
+    private UUID refreshTokenId;
+    private UUID token;
+    private boolean revoked;
+    private LocalDateTime expiresAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
