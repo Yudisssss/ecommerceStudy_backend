a58b7d262c0057b46b7be4ba83447a8490caffdc
Yudisssss <gabrielyudi06@gmail.com>
2026-09-05 02:07:04 -0300

    feat(src): create config for model maper use in application

--- src/main/java/com/ecommerce/study/config/BeansConfig.java
+++ src/main/java/com/ecommerce/study/config/BeansConfig.java
@@ -1,4 +1,12 @@
 package com.ecommerce.study.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeansConfig {
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
